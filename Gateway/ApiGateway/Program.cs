488c45c18bc6d7b37114aa0fb66d31c89136368d
Artem Bogdanovich <bogdanovicha898@gmail.com>
2025-07-14 15:49:54 +0300

    Remove blank lines

--- Gateway/ApiGateway/Program.cs
+++ Gateway/ApiGateway/Program.cs
@@ -8,29 +8,22 @@ namespace ApiGateway
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
-
             builder.Services.AddControllers();
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-
             builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
             builder.Services.AddOcelot();
 
             var app = builder.Build();
-
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
-
             app.UseHttpsRedirection();
             app.UseAuthorization();
             app.MapControllers();
-
             app.UseOcelot().Wait();
-
             app.Run();
         }
     }
