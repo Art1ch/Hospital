771617f3e72e315c587e5e16009d12be42ff1c6c
Artem Bogdanovich <bogdanovicha898@gmail.com>
2025-08-01 18:24:10 +0300

    Fix Remote Caller. Now if given list is empty, it won't call remote procedures. Move some operations in Remote Caller to private methods

--- Services/AppointmentAPI/AppointmentAPI.Infrastructure/Services/RemoteCaller/RemoteCaller.cs
+++ Services/AppointmentAPI/AppointmentAPI.Infrastructure/Services/RemoteCaller/RemoteCaller.cs
@@ -11,20 +11,36 @@ internal sealed class RemoteCaller(
 {
     public async Task<IEnumerable<string>> GetDoctorsEmailsAsync(IEnumerable<Guid> doctorsIds)
     {
+        if (!doctorsIds.Any() || doctorsIds == null)
+        {
+            return new List<string>();
+        }
+
         var stringDoctorsIds = doctorsIds.Select(x => x.ToString());
 
-        var getDoctorsAccountsIdsRequest = new GetDoctorsAccountsIdsRequest();
-        getDoctorsAccountsIdsRequest.DoctorsIds.AddRange(stringDoctorsIds);
-
+        var getDoctorsAccountsIdsRequest = GetDoctorsAccountsIdsRequest(stringDoctorsIds);
         var getDoctorsAccountsIdsResponse = await doctorServiceClient.GetDoctorsAccountsIdsAsync(getDoctorsAccountsIdsRequest);
 
-        var getAccountsEmailRequest = new GetAccountsEmailsRequest();
-        getAccountsEmailRequest.AccountsIds.AddRange(getDoctorsAccountsIdsResponse.AccountsIds);
-
+        var getAccountsEmailRequest = GetAccountsEmailsRequest(getDoctorsAccountsIdsResponse.AccountsIds);
         var getAccountsEmailResponse = await authServiceClient.GetAccountsEmailsAsync(getAccountsEmailRequest);
 
         var emails = getAccountsEmailResponse.Emails.ToList();
-
         return emails;
+    }
+
+    private GetDoctorsAccountsIdsRequest GetDoctorsAccountsIdsRequest(IEnumerable<string> stringDoctorsIds)
+    {
+        var getDoctorsAccountsIdsRequest = new GetDoctorsAccountsIdsRequest();
+        getDoctorsAccountsIdsRequest.DoctorsIds.AddRange(stringDoctorsIds);
+
+        return getDoctorsAccountsIdsRequest;
+    }
+
+    private GetAccountsEmailsRequest GetAccountsEmailsRequest(IEnumerable<string> accountsIds)
+    {
+        var getAccountsEmailRequest = new GetAccountsEmailsRequest();
+        getAccountsEmailRequest.AccountsIds.AddRange(accountsIds);
+
+        return getAccountsEmailRequest;
     }
 }
